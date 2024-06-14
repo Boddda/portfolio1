b159eea19fbc41ec0d7ba4643843704f2000a33c
Boddda <abdallahembaby816@gmail.com>
2024-06-14 22:15:55 +0900

    Add files via upload

---
+++ port1.js
@@ -0,0 +1,15 @@
+const porthead =document.querySelector('.porthead');
+const openMenu =document.querySelector('.openMenu');
+const closeMenu =document.querySelector('.closeMenu');
+
+openMenu.addEventListener('click',show);
+closeMenu.addEventListener('click',close);
+
+function show() {
+    porthead.style.display='flex';
+    porthead.style.right= '0';
+}
+
+function close() {
+    porthead.style.right= '-100%';
+}
